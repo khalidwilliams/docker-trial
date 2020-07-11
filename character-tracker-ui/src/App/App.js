532f3d6bd39a8a6e9dc78c65b5704880e1f492b3
Khalid Williams <khalidwill27@gmail.com>
2020-07-11 17:42:49 -0600

    Add character form

--- character-tracker-ui/src/App/App.js
+++ character-tracker-ui/src/App/App.js
@@ -1,12 +1,20 @@
 import { hot } from 'react-hot-loader/root';
 import React, {useState} from 'react';
 import Characters from '../Characters/Characters';
+import CharacterForm from '../CharacterForm/CharacterForm';
 
 const App = () => {
-  const [ characters, addCharacter ] = useState([]);
+  const [ characters, addCharacterToState ] = useState([]);
+
+  const addCharacter = (newCharacter) => {
+    const newCharacters = [...characters, newCharacter];
+    addCharacterToState(newCharacters)
+  }
+
   return (
     <main>
       <h1>Character Tracker</h1>
+      <CharacterForm currentCharacters={characters} addCharacter={addCharacter}/>
       <Characters characters={characters}/>
     </main>
   )
--- character-tracker-ui/src/Characters/Characters.js
+++ character-tracker-ui/src/Characters/Characters.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Characters.css';
 import Character from '../Character/Character';
 
 const Characters = ({ characters }) => {
